4b744113503cbf8c8e99ae7e31007f27407cb4b1
Đỗ Minh Hiếu <minhhieu.do@vietnamnet.vn>
2023-01-31 15:45:42 +0700

    update

--- api/product.js
+++ api/product.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const ProductCtrl = require("../controllers/ProductController");
+const { authMdw } = require("../middlewares/auth");
+const responseMessage = require("../message");
 
-router.get("/", async (req, res) => {
+router.get("/suggestions", async (req, res) => {
+  // try {
+  //   res.json({
+  //     status: 200,
+  //     message: "Get data has successfully",
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  //   return res.status(500).send("Server error");
+  // }
   try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
+    const randomProducts = await ProductCtrl.getRandomProducts(req.body.id);
+      res.json(randomProducts);
+  } catch (err) {
+      console.log(err);
+      res.json(responseMessage("error",1,''));
   }
 });
 
--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const express = require("express");
-const app = express();
 const product = require("./api/product");
+const { connectToDb } = require("./database");
+const router = require("./routers");
+const cors = require('cors')
 
+const app = express();
+
+app.use(cors({origin: "*"}));
 app.use(express.json({ extended: false }));
 app.use("/api/product", product);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
-// const express = require("express");
-// const { connectToDb } = require("./database");
-// const router = require("./routers");
-// const cors = require('cors')
 
-// const app = express();
-// app.use(
-//     cors({
-//         origin: "*"
-//     })
-// );
 // app.use(express.json());
 
 // app.use(router);
+connectToDb();
 
-// connectToDb();
-// const port = process.env.PORT || 5001;
-// app.listen(port, () => {
-//     console.log('App is running at ' + port);
-// });
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('App is running at ' + port);
+});
