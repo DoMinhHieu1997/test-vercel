b60f41d884c8220a61b5048245d94268feb33196
Đỗ Minh Hiếu <minhhieu.do@vietnamnet.vn>
2023-01-31 15:06:13 +0700

    update

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// const express = require("express");
-// const app = express();
-// const product = require("./api/product");
-
-// app.use(express.json({ extended: false }));
-// app.use("/api/product", product);
-
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 const express = require("express");
-const { connectToDb } = require("./database");
-const router = require("./routers");
-const cors = require('cors')
-
 const app = express();
-app.use(
-    cors({
-        origin: "*"
-    })
-);
-app.use(express.json());
+const product = require("./api/product");
 
-app.use(router);
+app.use(express.json({ extended: false }));
+app.use("/api/product", product);
 
-connectToDb();
-const port = process.env.PORT || 5001;
-app.listen(port, () => {
-    console.log('App is running at ' + port);
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+// const express = require("express");
+// const { connectToDb } = require("./database");
+// const router = require("./routers");
+// const cors = require('cors')
+
+// const app = express();
+// app.use(
+//     cors({
+//         origin: "*"
+//     })
+// );
+// app.use(express.json());
+
+// app.use(router);
+
+// connectToDb();
+// const port = process.env.PORT || 5001;
+// app.listen(port, () => {
+//     console.log('App is running at ' + port);
+// });
